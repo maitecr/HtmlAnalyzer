c4ffddf260b8f4df5687006c33ba5b9bdff4da5f
Maitê <81782090+maitecr@users.noreply.github.com>
2024-03-30 11:27:42 -0300

    Update HtmlAnalyzer.java

--- src/HtmlAnalyzer.java
+++ src/HtmlAnalyzer.java
@@ -5,16 +5,14 @@ public class HtmlAnalyzer {
         Connected connected = new Connected();
         GetHtmlContent getHtmlContent = new GetHtmlContent();
         AnalyzingHtmlContent analyzingHtmlContent = new AnalyzingHtmlContent();
-        ScanUrl scanUrl = new ScanUrl();
         String result = "";
-        String url = scanUrl.scanUrl();
+        String url = args[0];
 
         try {
             if(connected.isConnected(url)){
                  String htmlContent = getHtmlContent.getPage(url);
                  result = analyzingHtmlContent.analyzingHtmlContent(htmlContent);
                 System.out.println(result);
-                 //System.out.println(htmlContent);
             } else {
                  System.out.println("URL connection error");
             }
